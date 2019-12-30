6685b5a40dab9ff45a6ecc2aa2d3b449f7b5c7e6
H.P.Leung <kipperlhp@yahoo.com.hk>
2019-12-31 03:11:53 +0800

    project config customisation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'LEGO checker',
+    description: 'A checker for Lego sets',
+    author: 'H.P.Leung',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -20,12 +20,12 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'LEGO Checker',
+        short_name: 'LEGO Checker',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        background_color: '#000000',
+        theme_color: '#000000',
+        display: 'standalone',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
--- src/components/theme.js
+++ src/components/theme.js
@@ -1,13 +1,13 @@
 const theme = {
   palette: {
-    primary: ['#0F0F0F', '#2D2E2E', '#716969', '#BCABAE', '#FBFBFB'],
-    secondary: [],
-    black: ['#000000'],
+    primary: ['#000000', '#595959', '#7D7D7D', '#A8A8A8', '#D1D1D1'],
+    secondary: ['#FFCF00'],
     white: ['#FFFFFF'],
     grayscale: ['#212121', '#616161', '#9E9E9E', '#BDBDBD', '#E0E0E0', '#FFFFFF'],
+    error: ['#E3000B'],
   },
   fonts: {
-    primary: 'Segoe UI, Helvetica Neue, Helvetica, Roboto, sans-serif',
+    primary: 'Comic Sans, Segoe UI, Helvetica Neue, Helvetica, Roboto, sans-serif',
   },
   sizes: {
     mobile: '414px',
